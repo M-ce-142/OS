b2cd4c18bc1ffb3118bcd665deb9810b1bc5bdff
M-ce-142 <maance142@gmail.com>
2026-05-04 21:14:43 +0530

    Add date validation script
    This script validates a date input in dd-mm-yyyy format, checking for correct day, month, and leap year conditions.

---
+++ Date.sh
@@ -0,0 +1,38 @@
+echo "Enter date (dd-mm-yyyy):"
+read date
+
+dd=`echo $date | cut -d'-' -f1`
+mm=`echo $date | cut -d'-' -f2`
+yy=`echo $date | cut -d'-' -f3`
+
+if [ ${#dd} -ne 2 ] || [ ${#mm} -ne 2 ] || [ ${#yy} -ne 4 ]; then
+    echo "Invalid format"
+    exit
+fi
+
+echo $dd | grep -q '^[0-9][0-9]*$' || { echo "Invalid date"; exit; }
+echo $mm | grep -q '^[0-9][0-9]*$' || { echo "Invalid date"; exit; }
+echo $yy | grep -q '^[0-9][0-9]*$' || { echo "Invalid date"; exit; }
+
+if [ $mm -lt 1 ] || [ $mm -gt 12 ]; then
+    echo "Invalid month"
+    exit
+fi
+
+case $mm in
+    01|03|05|07|08|10|12) max=31 ;;
+    04|06|09|11) max=30 ;;
+    02)
+        if [ `expr $yy % 4` -eq 0 ]; then
+            max=29
+        else
+            max=28
+        fi
+        ;;
+esac
+
+if [ $dd -lt 1 ] || [ $dd -gt $max ]; then
+    echo "Invalid day"
+else
+    echo "Valid date"
+fi
