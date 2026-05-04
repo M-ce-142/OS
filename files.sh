d9ce81d1682018529a37548b510e1ccbceb53f87
M-ce-142 <maance142@gmail.com>
2026-05-04 21:25:08 +0530

    Fix file existence check in files.sh

--- files.sh
+++ files.sh
@@ -4,13 +4,11 @@ read f1
 echo "Enter second file name:"
 read f2
 
-# Check if both files exist
 if [ ! -f "$f1" ] || [ ! -f "$f2" ]; then
     echo "One or both files do not exist"
     exit
 fi
 
-# Compare files
 cmp -s "$f1" "$f2"
 
 if [ $? -eq 0 ]; then
